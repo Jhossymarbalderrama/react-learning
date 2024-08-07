1703a487a464e1f85fc2a74b2724e15639fadad5
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-07 15:44:28 -0300

    3- Validacion de datos de formularios

--- src/components/MiFormulario.jsx
+++ src/components/MiFormulario.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const MiFormulario = () => {
 
     /**
-     * ? Hook para informacion del Formulario
+     * ? Hook para capturar informacion del Formulario
      */
     const [formData, setFormData] = useState({
         username: '',
@@ -11,6 +11,16 @@ const MiFormulario = () => {
         password: '',
     });
     
+    /**
+     * ? Hook para capturar Errores en formulario (validaciones)
+     */
+    const [formErrors, setFormErrors] = useState({
+        username: '',
+        email:'',
+        password: '',
+    });
+
+
     /**
      * ? Capturo los inputs del formulario
      */
@@ -21,11 +31,45 @@ const MiFormulario = () => {
             ...prevFormData,
             [name]:value,
         }))
+
+        setFormErrors((prevFormErrors) => ({
+            ...prevFormErrors,
+            [name]:'',
+        }))
     }
     
     const procesarSubmit = (evento) => {
         evento.preventDefault();
-        console.log(formData);           
+        console.log(formData);  
+        
+        const newFormErrors = {}; // ? const donde guardo los errores si existe alguno
+
+        if(formData.username.trim() === ''){
+           newFormErrors.username = 'El usuario es requerido.';      
+        }
+
+        if(formData.email.trim() === ''){
+            newFormErrors.email = 'El email es requerido.';      
+         }
+
+         if(formData.password.trim() === ''){
+            newFormErrors.password = 'La contraseña es requerido.';      
+         }
+
+
+         /**
+          * ? Verifico si newFormErrors tiene errores
+          */
+         if(Object.keys(newFormErrors.length > 0))
+         {
+            setFormErrors(newFormErrors); // ? Seteo al hook de formErrors los errores
+         }else{
+            console.log('Formulario válido. Datos: ', formData);   // ? Todo joya. Formulario valido         
+         }
+        
+        
+        
+
     }
 
     /**
@@ -37,14 +81,18 @@ const MiFormulario = () => {
                 <div>
                     <label htmlFor="username">Usuario:</label>
                     <input type="text" id="username" name="username" value={formData.username} onChange={procesarCambio}/>
+
+                    {formErrors.username && <span className="error">*{formErrors.username}</span>}
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" value={formData.email} onChange={procesarCambio}/>
+                    {formErrors.email && <span className="error">*{formErrors.email}</span>}
                 </div>
                 <div>
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" name="password" value={formData.password} onChange={procesarCambio}/>
+                    {formErrors.password && <span className="error">*{formErrors.password}</span>}
                 </div>
 
                 <button type="submit">Enviar</button>

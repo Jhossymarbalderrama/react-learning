4808c5c6e0ccd56651e969e72343c05583b2a257
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-07 12:45:21 -0300

    2- Hasta Composicion de componentes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ContadorClase from './components/ContadorClase'
+
+function App() {
+  return (
+    <>
+    
+      <ContadorClase/>
+    </>
+  )
+}
+
+export default App

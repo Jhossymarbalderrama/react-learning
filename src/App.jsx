39257ef7595dbc13550c3f8a4314ea74fa8f54b9
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-07 13:24:16 -0300

    2- Comunicacion entre componentes de Padre a Hijo | viceversa

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react'
 import './App.css'
 import ComponenteA from './components/ComponenteA'
 import ComponenteB from './components/ComponenteB'
@@ -7,20 +8,33 @@ function App() {
   const miEmoji = "🛩️"
   const miBoton = "Buscar Destino"
 
+  const [mensaje, setMensaje] = useState("Viajes Aventura")
+
   const manejarClick = () => {
-    alert("Click desde el componente Padre")
+    setMensaje("Listo para viajar");
   }
+
+  const callback = useCallback(
+    (datos) => {
+      setMensaje(datos);
+    },[mensaje]
+  )
 
   return (
     <>
       <div className='box'>
         <ComponenteA
           emoji={miEmoji}
-          titulo="Viajes Aventura"
+          titulo={mensaje}
         />
         <ComponenteB
-          onClick = {manejarClick}
           textoBtn={miBoton}
+          onClick={manejarClick}
+        />
+
+        <ComponenteB
+          textoBtn="Callback"
+          onClick={callback}
         />
       </div>
     </>
--- src/components/ComponenteB.jsx
+++ src/components/ComponenteB.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 const ComponenteB = ({textoBtn, onClick}) => {
+    const miValor = "Datos desde componente hijo"; 
 
     return (
-        <button onClick={onClick}>{textoBtn}</button>
+        <button onClick={()=>{onClick(miValor)}}>{textoBtn}</button>
     )
 }
 

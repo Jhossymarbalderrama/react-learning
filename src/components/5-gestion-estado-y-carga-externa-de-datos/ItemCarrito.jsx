7ec492d0a1e699da87c72f6622ff6ed9071a7e05
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-08 15:03:18 -0300

    5- Modificacion de estado en React

--- src/components/5-gestion-estado-y-carga-externa-de-datos/ItemCarrito.jsx
+++ src/components/5-gestion-estado-y-carga-externa-de-datos/ItemCarrito.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React from 'react'
 
-const ItemCarrito = () => {
-
-    return ('')
+function ItemCarrito({producto,onClick}) {
+    return (
+        <>
+            <div className="card mt-3">
+                <div className="card-body">
+                    <h5 className="card-title">{producto.nombre}</h5>
+                    <p className="card-text">${producto.precio}</p>
+                    <button className="btn btn-danger" onClick={onClick}>Remover</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ItemCarrito

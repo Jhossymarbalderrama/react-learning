71d021184a60b60f7d7521e2d366409f6d3d804b
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-08 14:36:04 -0300

    5- Carga externa de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 
 import './App.css'
-import Catalogo from './components/4-listas-estructuras-de-datos/Catalogo'
+import ListaProductos from './components/5-gestion-estado-y-carga-externa-de-datos/ListaProductos'
+import ListaCarrito from './components/5-gestion-estado-y-carga-externa-de-datos/ListaCarrito'
+import { useEffect, useState } from 'react'
 
 function App() {
 
- 
+  const [data, setData] = useState([]);
+
+  /**
+   * useEffect: Solo se va ejecutar solo si...
+   * sucede algo (modifique, agrege, propiedad ('prop') etc)
+   * 
+   * prop: cuando se actualiza
+   * []: dejando solo esto, se actualiza cuando se carga el componente
+   */
+  useEffect( () => {
+    fetch('../public/productos.json')
+    .then( res => res.json())
+    .then( datos => setData(datos))
+    .catch(error => console.error('Error, fetching data: ', error))
+  }, [])
 
   return (
     <>
-      <Catalogo/>
+      <div className="row">
+        <ListaProductos  productos={data}/>
+        <ListaCarrito  productos={data}/>
+      </div>
     </>
   )
 }
---
+++ src/components/5-gestion-estado-y-carga-externa-de-datos/ListaProductos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListaProductos = ({productos}) =>{
+    return (<></>)
+}
+
+export default ListaProductos;

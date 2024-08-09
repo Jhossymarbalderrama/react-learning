6b325738cbe690a7d3d1910fed8188befa42d65d
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-09 13:31:00 -0300

    6- Estilos dinamicos en React

--- src/components/6-import-CSS-dentro-de-componentes-React/Libro/Libro.jsx
+++ src/components/6-import-CSS-dentro-de-componentes-React/Libro/Libro.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Libro.css';    // ? Vincular CSS arhivo css a jsx
 import styled from 'styled-components'; // ? Libreria Estilos para componentes
 
@@ -11,18 +11,22 @@ const Libro = ({ libro }) => {
             flex-basis: 100%;      
     `
 
+    const [miClase, setMiClase] = useState('default');
+
+
     return (
         <>
-            <div className="card col-md-3">
+            <div className={'card col-md-3 ' + miClase}>
                 <div className="card-body">
-                    <h5 className="card-title">
+                    <h5 className="card-title h-25">
                        {libro.titulo}
                     </h5>
-                    <p className="card-text">Publicado en {libro.publicado}</p>
+                    <p className="card-text h-25">Publicado en {libro.publicado}</p>
+                    <button className="btn btn-primary" onClick={ () =>{setMiClase('seleccionado')} }> Seleccionar </button>
                 </div>
             </div>
         </>
     )
 }
 
-export default Libro
+export default Libro;

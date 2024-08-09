5df313dd6055e935dc805eaa77c54ebf716ec702
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-09 15:03:34 -0300

    7- Navegacion programatica y redirecciones

--- src/components/7-Enrutamiento/About.jsx
+++ src/components/7-Enrutamiento/About.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Contents from "./Contents";
+import { useNavigate } from "react-router-dom";
 
 
 const About = () => {
+
+    const navegar = useNavigate(); 
+
+    const manejarNavegacion = () => {
+        navegar('/');
+    }
+
     return (
         <>
             <h1>About</h1>
@@ -10,6 +18,10 @@ const About = () => {
             <Contents />
             <Contents />
             <Contents />
+
+            <button onClick={manejarNavegacion}>
+                Ir a Home
+            </button>
         </>
     )
 }

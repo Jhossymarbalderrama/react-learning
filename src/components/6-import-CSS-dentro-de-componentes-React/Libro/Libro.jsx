31a552e1b4a13bc27a447fd309c4f32ce51ae9ff
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-09 13:07:32 -0300

    6-Gemera Componentes con estilos usando Styled Componentes

--- src/components/6-import-CSS-dentro-de-componentes-React/Libro/Libro.jsx
+++ src/components/6-import-CSS-dentro-de-componentes-React/Libro/Libro.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import './Libro.css';    // ? Vincular CSS arhivo css a jsx
+import styled from 'styled-components'; // ? Libreria Estilos para componentes
 
-const Libro = ({libro}) => {
+const Libro = ({ libro }) => {
+
+    const Titulo = styled.h2`    
+            display: block;
+            padding: 0;
+            margin: 0;
+            flex-basis: 100%;      
+    `
 
     return (
         <>
             <div className="item">
-                <h2>{libro.titulo}</h2>
+                <Titulo>{libro.titulo}</Titulo>
                 <span>Publicado en {libro.publicado}</span>
             </div>
         </>

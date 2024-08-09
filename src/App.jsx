da3a7eda4ff0d93b53e214ebc49d48e76106b217
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-09 16:02:09 -0300

    7- Estilos dinamicos en React

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
 import Home from './components/7-Enrutamiento/Home'
 import About from './components/7-Enrutamiento/About'
-import Contents from './components/7-Enrutamiento/Contents'
+import Producto from './components/7-Enrutamiento/Producto'
 
 function App() {
   return (
@@ -20,6 +20,9 @@ function App() {
             <li className='nav-item'>
               <NavLink to='/about' className='nav-link fw-bold'>About </NavLink>
             </li>
+            <li className='nav-item'>
+              <NavLink to='/producto' className='nav-link fw-bold'>Producto </NavLink>
+            </li>
           </ul>
         </nav>
 
@@ -29,6 +32,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
+            <Route path='/producto/:nombreParam' element={<Producto />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
